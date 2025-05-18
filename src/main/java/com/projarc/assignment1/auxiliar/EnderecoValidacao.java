cf5cb55f72128446f4fd22b56766696866d938b3
HenriqueKnack22 <h.knack@edu.pucrs.br>
2025-05-18 16:20:47 -0300

    adicao da interface falando com a classe abstrata

--- src/main/java/com/projarc/assignment1/auxiliar/EnderecoValidacao.java
+++ src/main/java/com/projarc/assignment1/auxiliar/EnderecoValidacao.java
@@ -2,22 +2,15 @@ package com.projarc.assignment1.auxiliar;
 
 import com.projarc.assignment1.dominio.entidades.OrcamentoModel;
 
-public abstract class EnderecoValidacao {
-    protected EnderecoValidacao next;
+public abstract class EnderecoValidacao implements IEndereco {
+    protected IEndereco next;
 
-    public EnderecoValidacao linkWith(EnderecoValidacao next) {
+    @Override
+    public IEndereco linkWith(IEndereco next) {
         this.next = next;
         return next;
     }
 
+    @Override
     public abstract void check(OrcamentoModel orcamento);
 }
-
-
-
-//pra usar a cadeia
-//EnderecoValidacao validacao = new PaisValidacao();
-//validacao.linkWith(new EstadoValidacao());
-
-
-//validacao.check(orcamentoModel); // Lança exceção se não for atendido
